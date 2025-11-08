fd9483f2f43a568e8a498b8e1b3d483c7dc8e415
Vitaly Gashkov <vitalygashkov@vk.com>
2025-11-08 14:00:30 +0500

    fix: improve videoId parsing, improve episode links parsing

--- ntv.js
+++ ntv.js
@@ -4,21 +4,28 @@ export default defineExtension({
   async fetchContentMetadata(url) {
     const pageResponse = await fetch(url);
     const pageBody = await pageResponse.text();
-    const videoFrameLink = pageBody
-      .split(`<meta property="og:video:iframe" content="`)[1]
+    const videoMetadataFeedLink = pageBody
+      .split(`<meta property="ya:ovs:feed_url" content="`)[1]
       ?.split(`">`)[0]
       ?.split(`" />`)[0];
 
-    if (!videoFrameLink) {
+    if (!videoMetadataFeedLink) {
       const pathname = new URL(url).pathname;
       if (pathname.includes('/serial')) {
-        const parts = pageBody.split('wide-card-video" href="');
+        const parts = pageBody.split('href="/serial');
         const results = [];
         for (const part of parts) {
           if (!part.startsWith('/')) continue;
           const route = part.split('"')[0];
-          if (!route) continue;
-          const episodeUrl = url.replace(pathname, route);
+          if (
+            !route ||
+            route.length <= 1 ||
+            route.includes('/issues/') ||
+            route.includes('/about/') ||
+            route.split('/').length <= 2
+          )
+            continue;
+          const episodeUrl = url.replace(pathname, `/serial${route}`);
           const [metadata] = await this.fetchContentMetadata(episodeUrl);
           if (metadata) results.push(metadata);
         }
@@ -29,7 +36,7 @@ export default defineExtension({
       }
     }
 
-    const videoId = videoFrameLink.split(`embed/`)[1]?.split(`/`)[0];
+    const videoId = videoMetadataFeedLink.split(`video/`)[1]?.split(`/`)[0];
     const xmlResponse = await fetch(`https://www.ntv.ru/vi${videoId}/`);
     const xmlBody = await xmlResponse.text();
     const fileLink = xmlBody.split(`<file>`)[1]?.split(`</file>`)[0]?.split(`DATA[`)[1]?.split(`]`)[0];
